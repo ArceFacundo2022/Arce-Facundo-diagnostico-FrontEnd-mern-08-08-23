2ea4e91c2790d5e9cf937bd748c8f6c359a4ae01
ArceFacundo2022 <17luckgames17@gmail.com>
2023-08-08 16:34:40 -0300

    Desarrollo del cliente React

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+//import CardFetch from '../components/CardFetch'
+import { useState } from 'react'
+import '../style.css'
+
+function Home() {
+
+  const [show, setShow] = useState(false)
+
+  return (
+    <div className='row'>
+      <button type='button' className='buttMostrar btn btn-outline-danger' hidden={show} onClick={() => {setShow(true)}}>Mostrar Cartas</button>
+      <div className="card" hidden={!show} style={{"background-color" : "black"}}>
+        <div className='row' id='cardFetch'>
+          { show }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
